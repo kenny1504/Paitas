77ac4493728cafbc2ae84c02cd9e115c79ed07e4
kenny <kennysaenz31@gmail.com>
2026-04-01 16:36:03 -0600

    Update `APP_PATHS` in config to remove unused reels-related paths

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,8 @@
-export const SITE_BASE_URL = "https://radiofuturanicaragua.com";
+export const SITE_BASE_URL = "/proxy";
 
 export const APP_PATHS = {
   clientIndex: "/clientes/data/index.json",
   clientDataBase: "/clientes/data",
-  reelsIndex: "/clientes/reels/index.php",
-  reelsBase: "/clientes/reels",
   futuraLogo: "/images/logo-futura.png",
   tuSpotLogo: "/tuspotenlaradio/img/tuspot.png",
   sergioPhoto: "/tuspotenlaradio/futura/img/sergio.png",
