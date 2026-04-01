43d9e33d56ac60474cefdc926fc13ff38021e045
kenny <kennysaenz31@gmail.com>
2026-04-01 16:38:22 -0600

    Update `APP_PATHS` with reels paths and append trailing slash to `SITE_BASE_URL`

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,14 +1,15 @@
-export const SITE_BASE_URL = "/proxy";
+export const SITE_BASE_URL = "/proxy/";
 
 export const APP_PATHS = {
   clientIndex: "/clientes/data/index.json",
   clientDataBase: "/clientes/data",
+  reelsIndex: "/clientes/reels/index.php",
+  reelsBase: "/clientes/reels",
   futuraLogo: "/images/logo-futura.png",
   tuSpotLogo: "/tuspotenlaradio/img/tuspot.png",
   sergioPhoto: "/tuspotenlaradio/futura/img/sergio.png",
   jaquelinPhoto: "/tuspotenlaradio/futura/img/jaquelin.png"
 };
-
 export function buildSiteUrl(path) {
   return `${SITE_BASE_URL}${path}`;
 }
